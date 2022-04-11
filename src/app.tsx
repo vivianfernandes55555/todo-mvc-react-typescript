bfa8a3ceb60b13a63089173305950c7855498d15
vivianfernandes55555 <vivianfernandes55555@gmail.com>
2022-04-11 23:32:25 +0530

    added files

---
+++ src/app.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from "react";
+import "./app.css";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TodoContainer from "./app/containers/todo/todo.container";
+
+const App: FC = () => {
+  const routes = [
+    {
+      path: '/',
+      exact: true,
+      component: TodoContainer
+    }
+  ]
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          {routes.map((route, i) => {
+            return (<Route
+              key={route.path}
+              path={route.path}
+              exact={route.exact}
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              render={(props:any) => (
+                <route.component {...props} {...route} />
+              )}
+            >
+            </Route>
+            );
+          })}
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
